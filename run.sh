7e789f339d9e4143882e51a0122acf07841c5df6
GodsLeft <1456466573@qq.com>
2017-03-10 22:02:13 +0800

    small change

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 sbt package
 
-scp -P 13000 ./runspark.sh ./target/scala-2.10/sparktest_2.10-1.0.jar bigdata@slave04:~/
-# scp -P 13000 ./runspark.sh bigdata@slave04:~/
+scp -P 13000 ./runspark.sh \
+             ./target/scala-2.10/sparktest_2.10-1.0.jar \
+             bigdata@slave04:~/
 
 #ssh -p 13000 bigdata@slave04 "spark-submit \
 #                            --class sparktest \
