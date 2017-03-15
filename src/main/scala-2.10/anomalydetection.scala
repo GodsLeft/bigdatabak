d2c09c4a2348346338970f749da70764b38192b1
GodsLeft <1456466573@qq.com>
2017-03-15 17:06:38 +0800

    完成了异常检测算法，有一点bug

--- src/main/scala-2.10/anomalydetection.scala
+++ src/main/scala-2.10/anomalydetection.scala
@@ -6,22 +6,59 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by left on 17-3-15.
   */
 object anomalydetection {
+
+  // 公式中使用到的sigma是标准差
+  def gaosi(x: Double, u: Double, sigma: Double): Double = {
+    1.0 / sigma / Math.sqrt(2 * Math.PI) * Math.exp(- Math.pow(x - u, 2) / 2 / Math.pow(sigma, 2))
+  }
+
+  // 这边传递的是方差数组
+  def linegaosi(xarr: Array[Double], uarr: Array[Double], sarr: Array[Double]): Double = {
+    var result = 1.0
+    for(i <- 0 until xarr.length){
+      result *= gaosi(xarr(i), uarr(i), sarr(i))
+    }
+    result
+  }
+
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("statistics")
     val sc = new SparkContext(conf)
 
-    val observations = sc.parallelize(
-      Seq(
-        Vectors.dense(1.0, 10.0, 100.0),
-        Vectors.dense(2.0, 20.0, 200.0),
-        Vectors.dense(3.0, 30.0, 300.0)
-      )
-    )
+    val word = sc.textFile(kmeanstest.inputpath)
+      .flatMap(line => line.split(kmeanstest.regstring))
+      .filter(word => word.matches("[a-zA-Z]+"))
+      .distinct()
 
-    val summary = Statistics.colStats(observations)
-    println(summary.mean)
-    println(summary.variance)
-    println(summary.numNonzeros)
+    val lines = sc.textFile(kmeanstest.inputpath).count()
+
+    val vecbag = word.collect()
+
+    val hashmap = kmeanstest.wdchashmap(sc, kmeanstest.inputpath).toMap
+
+    val vecrdd = sc.textFile(kmeanstest.inputpath).map{line => Vectors.dense(kmeanstest.line2vec(line, vecbag, hashmap, lines))}
+
+    val stat = Statistics.colStats(vecrdd)
+
+    val mean = stat.mean
+    val variance = stat.variance
+    val sigma = variance.toArray.map(x => Math.sqrt(x))
+
+    // 对任一条数据，进行预测是否异常
+    // 暂时只输出异常的
+    sc.textFile(kmeanstest.inputpath).map{
+      line =>
+        val linevec = Vectors.dense(kmeanstest.line2vec(line, vecbag, hashmap, lines))
+        val lineresult = linegaosi(linevec.toArray, mean.toArray, variance.toArray)
+        //if (lineresult < 0.001) {
+        //  ("a", line)
+        //}
+        lineresult
+    }.saveAsTextFile("hdfs://master:9000/user/bigdata/yichang")
+
+    println("mean: " + mean)
+    println("vari: " + variance)
+    println("sigm: " + sigma.mkString(","))
   }
 
 }
