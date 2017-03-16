fde2aaa99af74d857f7debd0dfb656cd6ab6395a
GodsLeft <1456466573@qq.com>
2017-03-16 16:02:16 +0800

    免密码传输

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+
 sbt package
 
-scp -P 13000 ./runspark.sh \
+expect -c '''
+spawn scp -P 13000 ./runspark.sh \
              ./target/scala-2.10/sparktest_2.10-1.0.jar \
              bigdata@slave04:~/
+
+expect "*assword:*"
+send "xxxxxxxx\n"
+interact
+'''
 
 #ssh -p 13000 bigdata@slave04 "spark-submit \
 #                            --class wordcount \
