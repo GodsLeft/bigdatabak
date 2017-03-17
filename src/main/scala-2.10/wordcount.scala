afa0ca988a1c5759cb11d2e4f8cac4235c4adf3c
GodsLeft <1456466573@qq.com>
2017-03-17 20:18:28 +0800

    简单重构

--- src/main/scala-2.10/wordcount.scala
+++ src/main/scala-2.10/wordcount.scala
@@ -5,25 +5,32 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 
 object wordcount {
+  def wdcnt(sc: SparkContext, input: String): Long ={
+    sc.textFile(input).flatMap(line => line.split(util.regstring))
+      .map(w => (w, 1))
+      .reduceByKey(_+_)
+      .count
+  }
+
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("sparktest")
+    val conf = new SparkConf().setAppName("wordcount")
     val sc = new SparkContext(conf)
 
+    val hdfsfile = if (args(0) != null) args(0) else util.inputpath
+    val allufile = if (args(1) != null) args(1) else "alluxio://master:19998/user/bigdata/ips.csv"
+
     val starttime: Long = System.nanoTime();
-    val logfile = if (args(0) != null) args(0) else "hdfs://master:9000/user/bigdata/ips_10000.csv"
-    val file = sc.textFile(logfile)
-    val wordcount = file.flatMap(line => line.split(" ")).map(w => (w, 1)).reduceByKey(_+_).count
+    wdcnt(sc, hdfsfile)
     val time1 = System.nanoTime() - starttime
 
     println("============ alluxio start ==========")
 
-    // 使用了alluxio
     val starttime1: Long = System.nanoTime();
-    val logfile1 = if (args(1) != null) args(1) else "alluxio://master:19998/user/bigdata/ips_10000.csv"
-    val file1 = sc.textFile(logfile1)
-    val wordcount1 = file.flatMap(line => line.split(" ")).map(w => (w, 1)).reduceByKey(_+_).count
+    wdcnt(sc, allufile)
     val time2 = System.nanoTime() - starttime1
 
-    println("zhutime:" + time1 + " : " + time2)
+    println("zhutime: " + time1 + " : " + time2)
+
+    sc.stop()
   }
 }
