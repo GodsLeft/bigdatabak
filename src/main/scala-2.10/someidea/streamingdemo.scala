f7578b21f4b20436fef13f51967464d6d01fe038
GodsLeft <1456466573@qq.com>
2017-04-03 15:33:26 +0800

    模拟数据流

---
+++ src/main/scala-2.10/someidea/streamingdemo.scala
@@ -0,0 +1,8 @@
+package someidea
+
+/**
+  * Created by left on 17-4-2.
+  */
+class streamingdemo {
+
+}
---
+++ streamingmock.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# 为了模拟数据流，每秒中输出1行日志文本
+while read LINE
+do
+    sleep 1
+    echo $LINE
+done < ./ips.csv | nc -lk 9999
