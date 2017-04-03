079cf059d829a62bdf1b4d391e561b230a99a454
GodsLeft <1456466573@qq.com>
2017-04-04 00:00:52 +0800

    日志分析

--- src/main/scala-2.10/someidea/streamingdemo.scala
+++ src/main/scala-2.10/someidea/streamingdemo.scala
@@ -13,11 +13,37 @@ object streamingdemo {
     val ssc = new StreamingContext(conf, Seconds(1))
 
     val lines = ssc.socketTextStream("localhost", 9999)
-    val words = lines.flatMap(_.split(" "))
+    val ipreg = """((?:(?:25[0-5]|2[0-4]\d|(?:1\d{2}|[1-9]?\d))\.){3}(?:25[0-5]|2[0-4]\d|(?:1\d{2}|[1-9]?\d)))""".r
 
-    val pairs = words.map(word => (word, 1))
-    val wordCounts = pairs.reduceByKey(_ + _)
-    wordCounts.print()
+    val result = lines
+      .map{line =>
+        val words = line.split(",")
+        val srcips = words.filter(word => word.contains("srcip"))
+        val dstips = words.filter(word => word.contains("dstip"))
+        if(srcips.length > 0 && dstips.length > 0){
+          val srcip = "ip"+ipreg.findFirstIn(srcips(0)).getOrElse("none").replace('.', 'x')
+          val dstip = "ip"+ipreg.findFirstIn(dstips(0)).getOrElse("none").replace('.', 'x')
+          (srcip + " -> " + dstip, 1)
+        } else {
+          ("_", 1)
+        }
+      }
+      .reduceByKey(_+_)
+      .map{line =>
+        val key = line._1
+        val value = "[ label = \"" + line._2 + "\" ]"
+        key + " " + value + " ;"
+      }
+      .repartition(1)
+        .foreachRDD(_.saveAsTextFile("file:///home/bigdata/streaming"))
+      //.saveAsTextFiles(prefix="file:///home/bigdata/streaming", suffix = "dot")
+
+
+    //val words = lines.flatMap(_.split(","))
+
+    //val pairs = words.map(word => (word, 1))
+    //val wordCounts = pairs.reduceByKey(_ + _)
+    //wordCounts.print()
 
     ssc.start() //开始计算
     ssc.awaitTermination() //等待计算终结
