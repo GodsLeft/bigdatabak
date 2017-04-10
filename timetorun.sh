8a57937881aa54cc1217f40eb4e8610ebf8c6ef8
GodsLeft <1456466573@qq.com>
2017-04-10 23:26:50 +0800

    定时执行runspark

--- timetorun.sh
+++ timetorun.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
-hdfspath=hdfs://master:9000/user/bigdata
-allupath=alluxio://master:19998/user/bigdata
-
-runkmeanstest(){
-    for index in {0..5};do
-        echo "===="$index"====" >> kmeanstime
-        hdfsinput=$hdfspath/ipsdata/ips_$index.csv
-        for cnt in {0..9};do
-            hadoop fs -rm -R kmeans
-            #alluxio fs rm -R /user/bigdata/kmeans
-            hdfsout=$hdfspath/kmeans
-            { time -p spark-submit \
-                        --class kmeans \
-                        --master spark://master:7077 \
-                        --executor-memory 20G \
-                        sparktest*.jar \
-                        $hdfsinput \
-                        $hdfsout 2> /dev/null;
-            } 2>> kmeanstime
-        done
-    done
-}
-# 可以让我在半夜执行一个任务
+# 暂时先这样写
 while [ true ];do
-    sleep 10
+    sleep 600
     ppnum=$(ps aux | grep "runspark" | wc -l)
-    if [ $ppnum -ge 2 ];then
-        #sed -i ""  # 切换任务的命令
-        #./runspark.sh
-        echo "hello"
+    if [ $ppnum -lt 2 ];then
+        mv kmeanstime kmeanstime00
+        ./runspark.sh
+        echo "done"
         break
     fi
 done
+# 可以让我在半夜监控一个任务完成，并执行一个任务
+#while [ true ];do
+#    sleep 10
+#    ppnum=$(ps aux | grep "runspark" | wc -l)
+#    if [ $ppnum -lt 2 ];then
+#        mv kmeanstime kmeanstime00
+#        sed -i "s/\$hdfspath/\$hdfspath/g" runkmeanstest.sh  # 切换任务的命令
+#        sed -i "s/hadoop fs/#hadoop fs/g" runkmeanstest.sh
+#        sed -i "s/#alluxio/alluxio/g" runkmeanstest.sh
+#        #sed -i "s/hadoop fs -rm -R kmeans/alluxio fs rm -R \/user\/bigdata\/kmeans/g" timetorun.sh
+#        ./runkmeanstest
+#        #./runspark.sh
+#        echo "hello"
+#        break
+#    fi
+#done
