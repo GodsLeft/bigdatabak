5aca783923297c331d47e8cdccbc91f17e7e8e3e
GodsLeft <1456466573@qq.com>
2017-04-13 15:45:44 +0800

    完成了动态增加

--- recivedot.sh
+++ recivedot.sh
@@ -2,6 +2,6 @@
 
 # 这个文件只是为了在别的机器上显示图片
 while [ true ];do
-    sleep 1
-    nc -l 20000 > srcdst.svg
+    sleep 0.1
+    nc -l 20000 > srcdst.png
 done
--- todot.sh
+++ todot.sh
@@ -14,6 +14,7 @@ while [ true ];do
         # 这边还缺少一个判断srcdst.dot文件是否存在的判断
         if [ ! -e streamingdot/srcdst.dot ]; then
             echo "digraph srcdst{" > streamingdot/srcdst.dot
+            echo 'graph[bgcolor="transparent"]' >> streamingdot/srcdst.dot  # 想让背景透明
             echo "}" >> streamingdot/srcdst.dot
         fi
 
@@ -21,15 +22,17 @@ while [ true ];do
         cat streamingdot/part-00000 >> streamingdot/srcdst.dot  # 將新文件添加到dot文件之後
         echo '}' >> streamingdot/srcdst.dot  # 添加最後一行
 
-        linenum=$(wc -l streamingdot/srcdst.dot)
-        if [ $linenum -gt 100 ]; then
-            decnum=$(($linenum - 100))
-            sed -i "2,$decnum" streamingdot/srcdst.dot
+        linenum=$(cat streamingdot/srcdst.dot | wc -l)
+        savelines=25
+        if [ $linenum -gt $savelines ]; then
+            decnum=$(($linenum - $savelines))
+            sed -i "3,${decnum}d" streamingdot/srcdst.dot
         fi
-        dot -Tsvg streamingdot/srcdst.dot -o streamingdot/srcdst.svg
+        #dot -Tsvg streamingdot/srcdst.dot -o streamingdot/srcdst.svg
+        dot -Tpng streamingdot/srcdst.dot -o streamingdot/srcdst.png
 
-        if [ -f streamingdot/srcdst.svg ]; then
-            cat streamingdot/srcdst.svg | nc slave04 20000
+        if [ -f streamingdot/srcdst.png ]; then
+            cat streamingdot/srcdst.png | nc 10.10.80.167 20000  # 这里的ip地址是内部ip地址
         fi
     fi
 
@@ -45,5 +48,5 @@ while [ true ];do
     #    if [ -f streamingdot/srcdst.svg ]; then
     #        cat streamingdot/srcdst.svg | nc slave04 20000 # 这里的slave04可以换成本机ubuntu的ip
     #    fi
-    fi
+    #fi
 done
