1139459897331bc020c03650bf356e941c1abd64
GodsLeft <1456466573@qq.com>
2017-06-08 21:37:02 +0800

    运行在slave03

--- src/main/scala-2.10/wordcount/runtest.sh
+++ src/main/scala-2.10/wordcount/runtest.sh
@@ -11,7 +11,7 @@ runwordcount(){
         --master $MASTERPATH \
         --executor-memory 20G \
         $BASEPATH/target/scala-2.10/sparktest*.jar \
-        hdfs://slave04:9000/user/bigdata/ips.csv
+        hdfs://master:9000/user/bigdata/ipsdata/ips.csv
 }
 
 time -p runwordcount
