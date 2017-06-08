c9fe15248fe74dfcac8dd7502af901bf55a4bdb1
GodsLeft <1456466573@qq.com>
2017-06-08 15:42:55 +0800

    上传自动输入密码

--- bin/pushhadoop03.sh
+++ bin/pushhadoop03.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# 一些上传用的脚本
 scp -P xxxx ./runspark.sh \
              ./cutdata.sh \
              ./todot.sh \
@@ -18,3 +19,21 @@ expect "*assword:*"
 send "password"
 interact
 '''
+
+
+#expect -c '''
+#spawn scp -P 40000 \
+#             ./startsparktest.sh \
+#             ./cutdata.sh \
+#             ./src/main/scala-2.10/streaming/todot.sh \
+#             ./src/main/scala-2.10/streaming/streamingmock.sh \
+#             ./src/main/scala-2.10/dataskew/makedataskew.sh \
+#             ./src/main/scala-2.10/alluxiostore/storetest.sh \
+#             ./tocsv.sh \
+#             ./target/scala-2.10/sparktest_2.10-1.0.jar \
+#             bigdata@slave04:~/sparktest/
+#
+#expect "*assword:*"
+#send "xxxxxxxxxx\n"
+#interact
+#'''
